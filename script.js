3266492eb6e313901852915cc85269d84a2ab71b
NaidelynHernandez <44040610+NaidelynHernandez@users.noreply.github.com>
2018-12-12 17:49:00 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$("#button").click(function(){
+    $("#aries").slideToggle();
+});
+
+$("#button").click(function(){
+    $("#aries").show();
+});
+
+$("#button1").click(function(){
+    $("#capricorn").slideToggle();
+});
+
+$("#button2").click(function(){
+    $("#virgo").slideToggle();
+});
